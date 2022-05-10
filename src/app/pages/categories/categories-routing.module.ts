4889fdc191893dee4d1598de22da7a8c7a4f0064
ElivelNascimento <elivelsantos81@gmail.com>
2022-05-10 17:45:32 -0300

    foi desenvolvido o layout da tela categoria

--- src/app/pages/categories/categories-routing.module.ts
+++ src/app/pages/categories/categories-routing.module.ts
@@ -6,7 +6,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {path: '', component: CategoryListComponent },
   { path: 'new', component: CategoryFormComponent },
-  { path: 'id/edit', component: CategoryFormComponent }
+  { path: ':id/edit', component: CategoryFormComponent }
 ];
 
 @NgModule({
