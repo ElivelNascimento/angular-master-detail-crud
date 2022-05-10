d47254d868978d99a5cc9a8fc9c870db72f444c2
ElivelNascimento <elivelsantos81@gmail.com>
2022-05-10 15:36:51 -0300

    ajuste no module de categories

---
+++ src/app/pages/categories/categories.module.ts
@@ -0,0 +1,21 @@
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CategoriesRoutingModule } from './categories-routing.module';
+import { CategoryListComponent } from './category-list/category-list.component';
+import { CategoryFormComponent } from './category-form/category-form.component';
+
+
+@NgModule({
+  declarations: [
+    CategoryListComponent,
+    CategoryFormComponent
+  ],
+  imports: [
+    CommonModule,
+    CategoriesRoutingModule, 
+    FormsModule
+  ]
+})
+export class CategoriesModule { }
